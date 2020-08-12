51907a028f6e6b75e5cbecd8eea6ab2df6d157a8
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-08-12 12:47:22 +0500

    Create Fire4b.jsx

---
+++ src/Fire4b.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+
+const Fire4b = () => {
+
+  return (
+
+    <div className="fire4-2">
+      <img src="/images-And-Sound/fire4-2.gif" alt="fire_image" />
+    </div>
+
+  );
+}
+export default Fire4b;
