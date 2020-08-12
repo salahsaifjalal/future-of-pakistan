feebab2c2992fc187f4804467129301b69ae51ff
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-08-12 12:49:21 +0500

    Create Ship12.js

---
+++ src/Ship12.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+
+const Ship12 = () => {
+
+
+
+    return (
+
+
+        <div className="ship12">
+            <img src="/images-And-Sound/ship12.gif" alt="shipImage" />
+        </div>
+    );
+}
+export default Ship12;
