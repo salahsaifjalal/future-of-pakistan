7ddf9889c5d354fec5cf76f01100e0307168e389
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-08-12 12:48:12 +0500

    Create FlagIS.js

---
+++ src/FlagIS.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+
+const FlagIS = () => {
+
+
+
+  return (
+
+    <div className="flagIS">
+      <img src="/images-And-Sound/flagIS.gif" alt="flagIsrael" />
+    </div>
+
+  );
+}
+export default FlagIS;
