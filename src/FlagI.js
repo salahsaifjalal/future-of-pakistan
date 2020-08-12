69cf4a2773e12c2a959fcb664654966d675c7445
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-08-12 12:48:06 +0500

    Create FlagI.js

---
+++ src/FlagI.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+
+
+const FlagI = () => {
+
+
+
+  return (
+
+    <div className="flagI">
+      <img src="/images-And-Sound/flagI.gif" alt="flagIndia" />
+    </div>
+  );
+}
+export default FlagI;
