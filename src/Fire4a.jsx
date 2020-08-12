11a6cc1627717733e16c50baee7ef4009f2f03ca
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-08-12 12:47:16 +0500

    Create Fire4a.jsx

---
+++ src/Fire4a.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+
+
+const Fire4a = () => {
+
+
+  return (
+
+    <div className="fire4-1">
+      <img src="/images-And-Sound/fire4-1.gif" alt="fire_image" />
+    </div>
+
+  );
+}
+export default Fire4a;
