4258592da49ed56996ca7ebfc2937b29840d6b26
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-08-12 12:48:17 +0500

    Create FlagP.jsx

---
+++ src/FlagP.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+
+
+const FlagP = () => {
+
+
+
+  return (
+
+    <div className="flagP">
+      <img src="/images-And-Sound/flagP.gif" alt="flagPakistan" />
+    </div>
+  );
+}
+export default FlagP;
