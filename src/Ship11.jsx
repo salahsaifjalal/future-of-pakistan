b217647c7414712ca895009cb8aeb11e87eaf20b
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-08-12 12:49:16 +0500

    Create Ship11.jsx

---
+++ src/Ship11.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+
+const Ship11 = () => {
+
+
+
+  return (
+
+    <div className="ship11">
+      <img src="/images-And-Sound/ship11.gif" alt="shipImage" />
+    </div>
+
+  );
+}
+export default Ship11;
