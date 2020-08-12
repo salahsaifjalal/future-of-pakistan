df988bf395a66e9df57c292b3b0ca67f153a4ec3
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-08-12 12:47:35 +0500

    Create Fire5.js

---
+++ src/Fire5.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+
+const Fire5 = () => {
+
+
+
+  return (
+
+    <div className="fire5">
+      <img src="/images-And-Sound/fire5.gif" alt="fire_image" />
+    </div>
+
+  );
+}
+export default Fire5;
