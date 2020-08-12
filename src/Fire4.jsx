84448563283c779ca3cee8890ccb4e3b29726402
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-08-12 12:47:11 +0500

    Create Fire4.jsx

---
+++ src/Fire4.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+
+
+const Fire4 = () => {
+
+
+  return (
+
+    <div className="fire4">
+      <img src="/images-And-Sound/fire4.gif" alt="fire_image" />
+    </div>
+
+  );
+}
+export default Fire4;
