c5d1319f2c7bface449272924adef2eced505216
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-08-12 12:47:05 +0500

    Create Fire3.js

---
+++ src/Fire3.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+
+
+const Fire3 = () => {
+
+
+  return (
+
+    <div className="fire3">
+      <img src="/images-And-Sound/fire3.gif" alt="fire_image" />
+    </div>
+
+  );
+}
+export default Fire3;
