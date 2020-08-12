ea51847780dac85f283fd79f3dff8a6cfcc41b35
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-08-12 12:46:33 +0500

    Create Baner.jsx

---
+++ src/Baner.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+
+
+const Baner = () => {
+
+    return (
+
+        <>
+            <div className="baner">
+                <h2> الحمد للہ پاکستان نے اللہ کے فضل و کرم سے ہندوستان اور اسرائیل کا صفایا کردیا۔
+                </h2>
+            </div>
+
+        </>
+
+    );
+}
+export default Baner;
