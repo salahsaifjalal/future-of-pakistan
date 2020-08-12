b9a8065bdadb0fd583045478062b01d1d83e01b6
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-08-12 12:48:33 +0500

    Create Gun1.jsx

---
+++ src/Gun1.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+
+const Gun1 = () => {
+
+
+
+  return (
+
+    <div className="gun1">
+      <img src="/images-And-Sound/gun1.gif" alt="gunImage" />
+    </div>);
+}
+export default Gun1;
