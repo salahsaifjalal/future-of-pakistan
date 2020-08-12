9caea52d532a1c1e36782e80adad803b18e16c87
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-08-12 12:47:47 +0500

    Create Fire5b.jsx

---
+++ src/Fire5b.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+
+const Fire5b = () => {
+
+
+
+  return (
+
+    <div className="fire5b">
+      <img src="/images-And-Sound/fire5.gif" alt="fire_image" />
+    </div>
+
+  );
+}
+export default Fire5b;
