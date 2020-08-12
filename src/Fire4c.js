0829b94d52d033d0bdc5e02b1e6376568272cbd3
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-08-12 12:47:29 +0500

    Create Fire4c.js

---
+++ src/Fire4c.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+
+const Fire4c = () => {
+
+
+
+  return (
+
+    <div className="fire4-3">
+      <img src="/images-And-Sound/fire4-3.gif" alt="fire_image" />
+    </div>
+
+  );
+}
+export default Fire4c;
